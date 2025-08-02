4bfe5983fd990ba9cce084f0bc7681c06e11d503
SiddTheCoder <siddthecoder@gmail.com>
2025-08-02 08:36:51 +0545

    updated axiosInstance

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const liveUrl = "https://moodmate-ai-d2g5.onrender.com/api/v1";
+const localUrl = "http://localhost:7000/api/v1";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000/api/v1",
+  baseURL: liveUrl,
   withCredentials: true, // because you're using cookies for JWT
 });
 
