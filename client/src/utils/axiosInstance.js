e1ad38d0f5694d227984c0be9be6ecba7521ae71
SiddTheCoder <162303130+SiddTheCoder@users.noreply.github.com>
2025-08-13 10:54:14 +0545

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const liveUrl = "https://moodmate-ai-d2g5.onrender.com/api/v1";
+const liveUrl = "https://moodmate-ai-gexx.onrender.com/api/v1";
 const localUrl = "http://localhost:7000/api/v1";
 
 const axiosInstance = axios.create({
